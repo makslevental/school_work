48a2c84f9ca32833873957a15df7ff633ceaa4f5
max laptop <petrovovitch@gmail.com>
2014-08-26 14:12:37 -0400

    remove dots folder

--- config_files/.emacs.d/elpa/auto-complete/auto-complete-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; auto-complete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (auto-complete) "auto-complete" "auto-complete.el"
-;;;;;;  (21216 24879 99090 692000))
-;;; Generated autoloads from auto-complete.el
-
-(autoload 'auto-complete "auto-complete" "\
-Start auto-completion at current point.
-
-\(fn &optional SOURCES)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
-;;;;;;  (21216 24879 193523 973000))
-
-;;;***
-
-(provide 'auto-complete-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; auto-complete-autoloads.el ends here
