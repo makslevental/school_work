7fa31f617bdded6f636b1806ffafe90e9a827401
makslevental <maksim.levental@gmail.com>
2021-03-14 19:31:02 -0500

    code for plots and sampling experiments

--- winter2021/CMSC33581/sample_joins/main.py
+++ winter2021/CMSC33581/sample_joins/main.py
@@ -1,4 +1,5 @@
 import math
+import pickle
 from typing import List, NamedTuple
 
 import matplotlib.pyplot as plt
@@ -11,11 +12,33 @@ con = psycopg2.connect(
 )
 
 
+def get_freqs(tbl, col):
+    q = f"""SELECT {col},
+        COUNT({col})
+        FROM {tbl}
+        GROUP BY {col}
+    """
+    cur = con.cursor()
+    cur.execute(q)
+    counts = {v: cnt for v, cnt in cur.fetchall()}
+    cur.close()
+    return counts
+
+
 def get_column_names(table):
     cur = con.cursor()
     cur.execute(f"SELECT * FROM {table} LIMIT 0")
     names = [desc[0] for desc in cur.description]
     return names
+
+
+def get_common_col(tbl1, tbl2):
+    col_names_1 = get_column_names(tbl1)
+    col_names_2 = get_column_names(tbl2)
+    common_col = set(col_names_1) & set(col_names_2)
+    assert len(common_col) == 1
+    common_col = common_col.pop()
+    return common_col
 
 
 def get_all_indices_map(table):
@@ -36,6 +59,27 @@ def samp_indices(full_join_name, samp_name):
     return samp_indices, np.array(list(full_indices.values()))
 
 
+def sample_one(tbl):
+    cur = con.cursor()
+    q = f"""SELECT * FROM {tbl} WHERE ctid IN (SELECT ctid FROM {tbl} ORDER BY RANDOM() LIMIT 1)"""
+    cur.execute(q)
+    res = cur.fetchone()
+    cur.close()
+    if res is not None:
+        return res
+    else:
+        raise Exception("no sample")
+
+
+def sample_one_matching(tbl, col_name, val):
+    cur = con.cursor()
+    q = f"""SELECT * FROM {tbl} WHERE ctid IN (SELECT ctid FROM {tbl} WHERE {tbl}.{col_name}={val} ORDER BY RANDOM() LIMIT 1)"""
+    cur.execute(q)
+    res = cur.fetchone()
+    cur.close()
+    return res
+
+
 def sample_indices(full_join_name, samples):
     cur = con.cursor()
     full_indices = get_all_indices_map(full_join_name)
@@ -46,10 +90,12 @@ def sample_indices(full_join_name, samples):
     for sample in samples:
         q = f"""SELECT ctid FROM {full_join_name} WHERE {" AND ".join([f"{k}={v}" for k, v in sample.items()])}"""
         cur.execute(q)
-        ((ctid,),) = cur.fetchall()
-        samp_indices.append(full_indices[ctid])
+        res = cur.fetchone()
+        if res is not None:
+            (ctid,) = res
+            samp_indices.append(full_indices[ctid])
     cur.close()
-    return samp_indices, np.array(list(full_indices.values()))
+    return np.array(samp_indices), np.array(list(full_indices.values()))
 
 
 def kolmogorov_smirnov_test(rvs1: np.ndarray, rvs2: np.ndarray):
@@ -66,41 +112,66 @@ def kolmogorov_smirnov_test(rvs1: np.ndarray, rvs2: np.ndarray):
 
 def two_join(tbl_name, table_name_1, table_name_2, view_or_tbl="TABLE"):
     cur = con.cursor()
-    cur.execute(
-        f"""CREATE {view_or_tbl} {tbl_name} AS SELECT * FROM {table_name_1} NATURAL JOIN {table_name_2};"""
-    )
-    con.commit()
+    # sample rels are views
+    if view_or_tbl == "VIEW":
+        cur.execute(f"""DROP MATERIALIZED VIEW IF EXISTS {tbl_name} CASCADE;""")
+        cur.execute(
+            f"""CREATE MATERIALIZED VIEW {tbl_name} AS SELECT * FROM {table_name_1} NATURAL JOIN {table_name_2};"""
+        )
+        con.commit()
+    else:
+        cur.execute(
+            f"""CREATE TABLE IF NOT EXISTS {tbl_name} AS SELECT * FROM {table_name_1} NATURAL JOIN {table_name_2};"""
+        )
+        con.commit()
     cur.close()
     return tbl_name
 
 
 def n_join(tbl_name, rel_names, view_or_tbl="TABLE"):
     cur = con.cursor()
-    cur.execute(
-        f"""CREATE {view_or_tbl} {tbl_name} AS SELECT * FROM {" NATURAL JOIN ".join(rel_names)};"""
-    )
-    con.commit()
+    # sample rels are views
+    if view_or_tbl == "VIEW":
+        cur.execute(f"""DROP MATERIALIZED VIEW IF EXISTS {tbl_name} CASCADE;""")
+        cur.execute(
+            f"""CREATE MATERIALIZED VIEW {tbl_name} AS SELECT * FROM {" NATURAL JOIN ".join(rel_names)};"""
+        )
+        con.commit()
+    else:
+        cur.execute(
+            f"""CREATE TABLE IF NOT EXISTS {tbl_name} AS SELECT * FROM {" NATURAL JOIN ".join(rel_names)};"""
+        )
+        con.commit()
+
     q = f"""
-    CREATE INDEX {tbl_name}_complete_idx ON {tbl_name}({",".join(get_column_names(tbl_name))});
+        CREATE INDEX IF NOT EXISTS {tbl_name}_complete_idx ON {tbl_name}({",".join(get_column_names(tbl_name))});
     """
     cur.execute(q)
     con.commit()
     cur.close()
 
 
-def naive_sample_f(table_name, f):
+def get_n_rows(table_name):
     cur = con.cursor()
     cur.execute(f"SELECT COUNT(1) from {table_name};")
-    ((n_rows,),) = cur.fetchall()
+    (n_rows,) = cur.fetchone()
     cur.close()
+    return n_rows
 
+
+def naive_sample_f(table_name, f):
+    n_rows = get_n_rows(table_name)
     return naive_sample_n(table_name, math.ceil(f * n_rows))
 
 
 def naive_sample_n(table_name, n):
     cur = con.cursor()
+    # sample rels are views
+    cur.execute(f"""DROP MATERIALIZED VIEW IF EXISTS {table_name}_samp CASCADE;""")
+    con.commit()
+
     cur.execute(
-        f"""CREATE VIEW {table_name}_samp AS SELECT * FROM {table_name} WHERE ctid IN (SELECT ctid FROM {table_name} ORDER BY RANDOM() LIMIT {n});"""
+        f"""CREATE MATERIALIZED VIEW {table_name}_samp AS SELECT * FROM {table_name} WHERE ctid IN (SELECT ctid FROM {table_name} ORDER BY RANDOM() LIMIT {n});"""
     )
     con.commit()
     cur.close()
@@ -112,28 +183,100 @@ def wander_sample_n(rels, n):
 
     samples = []
     for i in range(n):
-        q = f"""SELECT * FROM {rels[0]} WHERE ctid IN (SELECT ctid FROM {rels[0]} ORDER BY RANDOM() LIMIT 1)"""
-        cur.execute(q)
-        res = dict(zip(get_column_names(rels[0]), cur.fetchone()))
+        res = dict(zip(get_column_names(rels[0]), sample_one(rels[0])))
 
         for rel in rels[1:]:
             col_names = get_column_names(rel)
             common_col = res.keys() & set(col_names)
             assert len(common_col) == 1
             common_col = common_col.pop()
+
             q = f"""SELECT * FROM {rel} WHERE ctid IN (SELECT ctid FROM {rel} WHERE {rel}.{common_col}={res[common_col]} ORDER BY RANDOM() LIMIT 1)"""
             cur.execute(q)
             row = cur.fetchone()
-            if row:
-                res |= dict(zip(col_names, row))
+            if row is not None:
+                res.update(dict(zip(col_names, row)))
             else:
                 break
         else:
             samples.append(res)
             continue
         # break
+    cur.close()
     return samples
 
+
+def olken_sample_two_n(table_name_1, table_name_2, n):
+    col_names_1 = get_column_names(table_name_1)
+    col_names_to_idx_1 = {name: idx for idx, name in enumerate(col_names_1)}
+    col_names_2 = get_column_names(table_name_2)
+    col_names_to_idx_2 = {name: idx for idx, name in enumerate(col_names_2)}
+    common_col = get_common_col(table_name_1, table_name_2)
+    m2 = get_freqs(table_name_2, common_col)
+    M = max(m2.values())
+
+    S = [col_names_1 + col_names_2]
+
+    while len(S) < n:
+        t1 = sample_one(table_name_1)
+        t2 = sample_one_matching(
+            table_name_2, common_col, t1[col_names_to_idx_1[common_col]]
+        )
+        while t2 is None:
+            t1 = sample_one(table_name_1)
+            t2 = sample_one_matching(
+                table_name_2, common_col, t1[col_names_to_idx_1[common_col]]
+            )
+        if m2[t2[col_names_to_idx_2[common_col]]] < np.random.randint(0, M):
+            continue
+
+        S.append(dict(zip(col_names_1, t1)))
+        S[-1].update(dict(zip(col_names_2, t2)))
+
+    return S
+
+def weighted_wr_sample(table, n, weights, weight_col_name):
+    col_names = get_column_names(table)
+    col_names_to_idx = {name: idx for idx, name in enumerate(col_names)}
+
+    cur = con.cursor()
+    W = 0
+    A = n*[None]
+
+    cur.execute(f"SELECT * FROM {table}")
+
+    row = cur.fetchone()
+    while row:
+        w = weights[row[col_names_to_idx[weight_col_name]]]
+        W += w
+        for j in range(len(A)):
+            if np.random.random() <= w/W:
+                A[j] = row
+
+        row = cur.fetchone()
+
+    cur.close()
+    return A
+
+
+def stream_sample_two(table_name_1, table_name_2, n):
+    col_names_1 = get_column_names(table_name_1)
+    col_names_to_idx_1 = {name: idx for idx, name in enumerate(col_names_1)}
+    col_names_2 = get_column_names(table_name_2)
+    col_names_to_idx_2 = {name: idx for idx, name in enumerate(col_names_2)}
+    common_col = get_common_col(table_name_1, table_name_2)
+    m2 = get_freqs(table_name_2, common_col)
+
+    table_1_samples = weighted_wr_sample(table_name_1, n, m2, common_col)
+    samples = [col_names_1 + col_names_2]
+    for t1 in table_1_samples:
+        v = t1[col_names_to_idx_1[common_col]]
+        t2 = sample_one_matching(table_name_2, common_col, v)
+
+        samples.append(dict(zip(col_names_1, t1)))
+        samples[-1].update(dict(zip(col_names_2, t2)))
+
+    return samples
 
 def gen_bens(n=int(1e6), n_digs=2) -> List[int]:
     chi1 = np.random.default_rng().chisquare(df=1, size=n)
@@ -148,12 +291,12 @@ def gen_rel(name, n=int(1e6), attr_names=None):
     bens = list(zip(*[gen_bens(n) for _ in attr_names], range(1, n + 1)))
     attr_names.append(f"{name}_ctid")
     cur = con.cursor()
-    q = f"""CREATE TABLE {name} ({",".join([f"{a} int" for a in attr_names])});"""
+    q = f"""CREATE TABLE IF NOT EXISTS {name} ({",".join([f"{a} int" for a in attr_names])});"""
     cur.execute(q)
     for attr_name in attr_names:
         cur.execute(
             f"""
-            CREATE INDEX {name}_{attr_name}_idx ON {name}({attr_name});
+            CREATE INDEX IF NOT EXISTS {name}_{attr_name}_idx ON {name}({attr_name});
         """
         )
     for tup in bens:
@@ -204,45 +347,93 @@ def plot_n_hists(hists: List[Hist], title="", labels=None):
     fig.show()
 
 
-def five_join_demo():
-    K, n, f = 5, 1000, 0.5
-
+def loop_all_exps(K, N, loop_body):
+    # K, N = 5, 1000
     for k in range(2, K):
-        prefix = f"tbl_{k}_{n}_{int(f * 100)}"
-        rel_names = gen_k_rel(k, n, prefix=prefix)
-        # rel_names = ["R0", "R1", "R2", "R3", "R4"]
+        for f in np.linspace(0.1, 0.9, 9):
+            prefix = f"tbl_{k}_{N}_{int(f * 100)}"
+            loop_body(k, N, f, prefix)
+
+    # K, N = 3, 10000
+    # for k in range(2, K):
+    #     for f in np.linspace(0.1, 0.9, 9):
+    #         prefix = f"tbl_{k}_{N}_{int(f * 100)}"
+    #         loop_body(k, N, f, prefix)
+
+
+def gen_exps():
+    def loop_body(k, N, f, prefix):
+        rel_names = gen_k_rel(k, N, prefix=prefix)
         n_join(f"{prefix}_full_join", rel_names)
-        n_join(f"{prefix}_approx_samp", [naive_sample_f(rel, f) for rel in rel_names])
-        samp_indxs, full_indxs = samp_indices(
-            f"{prefix}_full_join", f"{prefix}_approx_samp"
+
+    loop_all_exps(5, 1000, loop_body)
+    loop_all_exps(3, 10000, loop_body)
+
+
+def wander_join_exps():
+    def loop_body(k, N, f, prefix):
+        print(f"{k},{N},{f},{prefix}")
+        rels = [f"r{i}" for i in range(k)]
+        n_rows = get_n_rows(f"{prefix}_full_join")
+        samples = wander_sample_n(
+            [f"{prefix}_{r}" for r in rels], math.ceil(f * n_rows)
+        )
+        samp_indxs, full_indxs = sample_indices(f"{prefix}_full_join", samples[1:])
+        pickle.dump(
+            {"samp_indxs": samp_indxs, "full_indxs": full_indxs},
+            open(
+                f"/home/maksim/dev_projects/school_work/winter2021/CMSC33581/sample_joins/{prefix}_wanderjoin.p",
+                "wb",
+            ),
         )
 
-        plot_n_hists(
-            [gen_hist(np.asarray(samp_indxs)), gen_hist(full_indxs)],
-            labels=["samp", "true"],
+    loop_all_exps(5, 1000, loop_body)
+    loop_all_exps(3, 10000, loop_body)
+
+
+def olken_join_exps():
+    def loop_body(k, N, f, prefix):
+        print(f"{k},{N},{f},{prefix}")
+        rels = [f"r{i}" for i in range(k)]
+        n_rows = get_n_rows(f"{prefix}_full_join")
+        samples = olken_sample_two_n(
+            *[f"{prefix}_{r}" for r in rels], math.ceil(f * n_rows)
+        )
+        samp_indxs, full_indxs = sample_indices(f"{prefix}_full_join", samples[1:])
+        pickle.dump(
+            {"samp_indxs": samp_indxs, "full_indxs": full_indxs},
+            open(
+                f"/home/maksim/dev_projects/school_work/winter2021/CMSC33581/sample_joins/{prefix}_olkenjoin.p",
+                "wb",
+            ),
         )
 
-    # kolmogorov_smirnov_test(approx_samp["A1"].values, true_samp["A1"].values)
+    loop_all_exps(3, 10000, loop_body)
 
+def stream_sample_exps(N):
+    def loop_body(k, N, f, prefix):
+        print(f"{k},{N},{f},{prefix}")
+        rels = [f"r{i}" for i in range(k)]
+        n_rows = get_n_rows(f"{prefix}_full_join")
+        samples = stream_sample_two(
+            *[f"{prefix}_{r}" for r in rels], math.ceil(f * n_rows)
+        )
+        samp_indxs, full_indxs = sample_indices(f"{prefix}_full_join", samples[1:])
+        pickle.dump(
+            {"samp_indxs": samp_indxs, "full_indxs": full_indxs},
+            open(
+                f"/home/maksim/dev_projects/school_work/winter2021/CMSC33581/sample_joins/{prefix}_streamjoin.p",
+                "wb",
+            ),
+        )
 
-def wander_join_demo():
-    prefix = "tbl_4_1000_50"
-    samples = wander_sample_n([f"{prefix}_{t}" for t in ["r0", "r1", "r2", "r3"]], 100)
-    samp_indxs, full_indxs = sample_indices(f"{prefix}_full_join", samples[1:])
-
-    plot_n_hists(
-        [gen_hist(np.asarray(samp_indxs)), gen_hist(full_indxs)],
-        labels=["samp", "true"],
-    )
-
+    loop_all_exps(3, N, loop_body)
 
 if __name__ == "__main__":
-    wander_join_demo()
-    # n_join(gen_k_rel(k=3, n=100))
-    # print(naive_sample_f("join_table", 0.5))
-    # two_join_demo()
-    # five_join_demo()
-
-    # wander_sample_n()
-    # for _ in range(10):
-    #     demo()
+    # gen_exps()
+    # print(get_freqs("tbl_2_1000_10_r0", "A0"))
+    # olken_join_exps()
+    # freqs = get_freqs("tbl_2_1000_10_r1", "A1")
+    # print(weighted_wr_sample("tbl_2_1000_10_r0", 100, freqs, "a1"))
+    # print(stream_sample_two("tbl_2_1000_10_r0", "tbl_2_1000_10_r1", 10))
+    stream_sample_exps(10000)
